c1c4d3178aa68d1d406b278e0b96979be6879e08
SinaGolshan <sinagolshan20002@gmail.com>
2020-09-29 19:00:46 +0330

    .

--- practice/1/script.js
+++ practice/1/script.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded",
     function(event){
+
+
         document.querySelector("button").addEventListener("click",function(){
             $ajaxUtils.sendGetRequest("data/name.txt", function(request){
                 var name = request.responseText;
                 document.querySelector("#target").textContent = "Hello " + name + "!";
             })
         })
+
+        document.querySelector("button").addEventListener("click",function(){
+            $ajaxUtils.sendGetRequest("data/name.json", function(request){
+                var person = JSON.parse(request.responseText);
+                console.log(person);
+                document.querySelector("#target2").textContent = "Hello " + person.firstName + " " + person.lastName + "age " 
+                + person.age +  "!";
+            })
+        })        
+
+
+
     })
