fe10a18dfe8745944d7be331214608f7be5b0d5c
SinaGolshan <71258467+SinaGolshan@users.noreply.github.com>
2020-09-29 16:06:34 +0330

    Update script.js

--- Practice/1/script.js
+++ Practice/1/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded",
         document.querySelector("button").addEventListener("click",function(){
             $ajaxUtils.sendGetRequest("data/name.txt", function(request){
                 var name = request.responceText;
-                console.log(name);
+                console.log(request);
                 document.querySelector("#target").textContent = "Hello " + name + "!";
             })
         })
