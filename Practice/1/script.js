10e1c04506544e5919eb3ab09336baa46788df9f
SinaGolshan <sinagolshan20002@gmail.com>
2020-09-29 16:18:22 +0330

    changed

--- Practice/1/script.js
+++ Practice/1/script.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded",
         document.querySelector("button").addEventListener("click",function(){
             $ajaxUtils.sendGetRequest("data/name.txt", function(request){
                 var name = request.responceText;
-                console.log(name);
+                console.log("name: " + name);
+                console.log("request: "request);
+                console.log("responceText: "+request.responceText);
                 document.querySelector("#target").textContent = "Hello " + name + "!";
             })
         })
